233cc7ec0bc4dc190cd6fcba33b1181def07a965
Alexander <zander.g.shah@gmail.com>
2016-06-07 02:21:59 -0400

    Fixed it

--- DungeonCrawler/src/engine/LivingEntity.java
+++ DungeonCrawler/src/engine/LivingEntity.java
@@ -91,8 +91,8 @@ public abstract class LivingEntity implements Drawable
 		AABB tempX = hitbox.clone();
 		AABB tempY = hitbox.clone();
 
-		tempX.updatePosition(position.add(new Vector2D(speed.getX(), 0)));
-		tempY.updatePosition(position.add(new Vector2D(0, speed.getY())));
+		tempX.updatePosition(position.add(new Vector2D(speed.getX() + 1, 0)));
+		tempY.updatePosition(position.add(new Vector2D(0, speed.getY() + 1)));
 
 		Vector2D newSpeed = new Vector2D(0, 0);
 
