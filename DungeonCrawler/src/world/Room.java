ef8e2b6a5e71984bf6485d0f706f0d0a0e270a8d
ZanderShah <zander.g.shah@gmail.com>
2016-06-10 12:40:03 -0400

    Doors went back to normal for some reason

--- DungeonCrawler/src/world/Room.java
+++ DungeonCrawler/src/world/Room.java
@@ -311,7 +311,7 @@ public class Room // implements Drawable (There should be 2 Drawable, one with
 		if (isCleared())
 			for (int i = 1; i < doors.length; i++)
 				if (doors[i] != null)
-					g.drawImage(doors[i].image(), doors[i].x(), doors[i].y(), null);
+					doors[i].draw(g, offset);
 
 		for (int i = 0; i < particles.size(); i++) {
 			particles.get(i).draw(g, offset);
