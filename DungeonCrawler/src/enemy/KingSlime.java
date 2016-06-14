b3ec7d710c8761c6da949bb155f907abf9e51785
Callum Moseley <callum.moseley1@gmail.com>
2016-06-14 11:56:05 -0400

    King slime gets faster when smaller

--- DungeonCrawler/src/enemy/KingSlime.java
+++ DungeonCrawler/src/enemy/KingSlime.java
@@ -9,18 +9,18 @@ import engine.AABB;
 import engine.Stats;
 import engine.damage.MeleeEnemyDamageSource;
 
-public class KingSlime extends Enemy
-{
+public class KingSlime extends Enemy {
 	private int movementCounter;
 	private Vector2D slideDir;
 
 	private int level;
+	private int waitTime;
+	private int slideTime;
 
-	public KingSlime(int x, int y, int l)
-	{
+	public KingSlime(int x, int y, int l) {
 		super();
 
-		setStats(new Stats(1000 / (int) (Math.pow(2, l)), 100, 100, 3, 30.0));
+		setStats(new Stats(1000 / (int) (Math.pow(2, l)), 100, 100, 3 * Math.pow(1.2, l), 30.0));
 		level = l;
 		setHitbox(new AABB(getPos().add(
 				new Vector2D(getWidth() / 2, getHeight() / 2)), getWidth(),
@@ -28,21 +28,20 @@ public class KingSlime extends Enemy
 		setPos(new Vector2D(x, y));
 		movementCounter = (int) (Math.random() * 150);
 		slideDir = new Vector2D();
+		slideTime = (int) (60 / Math.pow(1.2, l));
+		waitTime = 240;
 	}
 
 	@Override
-	public void addDamage(Room r)
-	{
+	public void addDamage(Room r) {
 		setDamageSource(new MeleeEnemyDamageSource(getHitbox(),
 				50 / (int) (Math.pow(2, level)), 30));
 		super.addDamage(r);
 	}
 
 	@Override
-	public void onDeath(Room r)
-	{
-		if (level < 3)
-		{
+	public void onDeath(Room r) {
+		if (level < 3) {
 			KingSlime left = new KingSlime((int) getPos().getX(),
 					(int) getPos().getY(), level + 1);
 			KingSlime right = new KingSlime((int) getPos().getX(),
@@ -55,23 +54,18 @@ public class KingSlime extends Enemy
 	}
 
 	@Override
-	public void update(Room l)
-	{
-		if (movementCounter == 0)
-		{
-			movementCounter = (int) (Math.random() * 60 + 240);
+	public void update(Room l) {
+		if (movementCounter == 0) {
+			movementCounter = waitTime;
 			setSpeed(new Vector2D());
 		}
-		else
-		{
+		else {
 			movementCounter--;
-			if (movementCounter == 60)
-			{
+			if (movementCounter == slideTime) {
 				slideDir = new Vector2D((int) (Math.random() * 360));
 				slideDir.multiplyBy(getStats().getSpeed());
 			}
-			if (movementCounter < 60)
-			{
+			if (movementCounter < slideTime) {
 				setSpeed(slideDir);
 			}
 		}
@@ -80,20 +74,17 @@ public class KingSlime extends Enemy
 	}
 
 	@Override
-	public int getWidth()
-	{
+	public int getWidth() {
 		return SpriteSheet.KING_SLIME[level].getWidth(null);
 	}
 
 	@Override
-	public int getHeight()
-	{
+	public int getHeight() {
 		return SpriteSheet.KING_SLIME[level].getHeight(null);
 	}
 
 	@Override
-	public void draw(Graphics g, Vector2D offset)
-	{
+	public void draw(Graphics g, Vector2D offset) {
 		Vector2D shifted = getPos().add(offset);
 		g.drawImage(SpriteSheet.KING_SLIME[level], (int) shifted.getX()
 				- getWidth() / 2, (int) shifted.getY() - getHeight() / 2, null);
