508f8ce063ced21e316bee48d4d88bd173d54d20
073725830 <073725830@316R20386LHFHX1.EDU.YRDSB.NET>
2016-06-14 12:06:15 -0400

    tank and warrior sprites

--- DungeonCrawler/src/app/ClientMain.java
+++ DungeonCrawler/src/app/ClientMain.java
@@ -34,9 +34,11 @@ import utility.Vector2D;
 import world.DungeonFactory;
 import world.Room;
 
-public class ClientMain extends JFrame {
+public class ClientMain extends JFrame
+{
 
-	public ClientMain() {
+	public ClientMain()
+	{
 		super("Dungeon Crawler");
 
 		Game gc = new Game();
@@ -46,27 +48,32 @@ public class ClientMain extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		gc.startGraphics();
-		if (Constants.OFFLINE) {
+		if (Constants.OFFLINE)
+		{
 			gc.startGame();
 		}
-		else {
-			try {
-				gc.connect(InetAddress.getByName("localhost"));
+		else
+		{
+			try
+			{
+				gc.connect(InetAddress.getByName("10.242.161.92"));
 			}
-			catch (Exception e) {
+			catch (Exception e)
+			{
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpriteSheet.initializeImages();
 		ClientMain mt = new ClientMain();
 		mt.setVisible(true);
 	}
 
-	static void drawMinimap(Room t, Graphics g, Vector2D offset,
-			boolean[] vis) {
+	static void drawMinimap(Room t, Graphics g, Vector2D offset, boolean[] vis)
+	{
 		if (t == null || vis[t.id()])
 			return;
 
@@ -80,7 +87,8 @@ public class ClientMain extends JFrame {
 	}
 
 	static class Game extends Canvas implements MouseListener,
-			MouseMotionListener, KeyListener {
+			MouseMotionListener, KeyListener
+	{
 		private static final int REC_PACKET_SIZE = 5000;
 
 		private ControlState cs;
@@ -95,25 +103,30 @@ public class ClientMain extends JFrame {
 		private ByteArrayOutputStream byteStream;
 		private ObjectOutputStream os;
 
-		public Game() {
+		public Game()
+		{
 			currentFloor = 0;
 			current = DungeonFactory.generateMap(Constants.NUMBER_OF_ROOMS, 0,
 					Constants.NUMBER_OF_FLOORS, (new Random()).nextLong());
 			current[currentFloor].setCurrent();
 
-			try {
+			try
+			{
 				sock = new DatagramSocket(Constants.CLIENT_PORT);
 			}
-			catch (SocketException e) {
+			catch (SocketException e)
+			{
 				e.printStackTrace();
 			}
 
 			byteStream = new ByteArrayOutputStream(5000);
-			try {
+			try
+			{
 				os = (new ObjectOutputStream(
 						new BufferedOutputStream(byteStream)));
 			}
-			catch (IOException e) {
+			catch (IOException e)
+			{
 				e.printStackTrace();
 			}
 
@@ -127,9 +140,11 @@ public class ClientMain extends JFrame {
 			cs = new ControlState();
 		}
 
-		public void connect(InetAddress i) {
+		public void connect(InetAddress i)
+		{
 			addr = i;
-			try {
+			try
+			{
 				sock.send(new DatagramPacket(new byte[] { 0 }, 1, i,
 						Constants.SERVER_PORT));
 				sock.send(new DatagramPacket(new byte[] { 1, 3 }, 2, i,
@@ -137,21 +152,26 @@ public class ClientMain extends JFrame {
 				sock.send(new DatagramPacket(new byte[] { 2 }, 1, i,
 						Constants.SERVER_PORT));
 			}
-			catch (Exception e) {
+			catch (Exception e)
+			{
 				e.printStackTrace();
 			}
 
 			(new Thread() {
 				@Override
-				public void run() {
-					while (true) {
+				public void run()
+				{
+					while (true)
+					{
 						DatagramPacket dp = new DatagramPacket(
 								new byte[REC_PACKET_SIZE], REC_PACKET_SIZE);
-						try {
+						try
+						{
 							sock.receive(dp);
 							parsePacket(dp);
 						}
-						catch (Exception e) {
+						catch (Exception e)
+						{
 							e.printStackTrace();
 						}
 					}
@@ -159,19 +179,26 @@ public class ClientMain extends JFrame {
 			}).start();
 		}
 
-		public void startGraphics() {
+		public void startGraphics()
+		{
 			createBufferStrategy(2);
 
 			(new Thread() {
-				public void run() {
+				public void run()
+				{
 					long lastUpdate = System.currentTimeMillis();
-					while (true) {
-						do {
-							do {
+					while (true)
+					{
+						do
+						{
+							do
+							{
 								Graphics graphics = Game.this
 										.getBufferStrategy().getDrawGraphics();
-								if (inGame) {
-									if (controlled != null) {
+								if (inGame)
+								{
+									if (controlled != null)
+									{
 										drawGame(graphics, controlled);
 									}
 								}
@@ -185,10 +212,12 @@ public class ClientMain extends JFrame {
 								.contentsLost());
 						long time = System.currentTimeMillis();
 						long diff = time - lastUpdate;
-						try {
+						try
+						{
 							Thread.sleep(Math.max(0, 1000 / 60 - diff));
 						}
-						catch (Exception e) {
+						catch (Exception e)
+						{
 							e.printStackTrace();
 						}
 					}
@@ -196,9 +225,11 @@ public class ClientMain extends JFrame {
 			}).start();
 		}
 
-		public void startGame() {
+		public void startGame()
+		{
 			inGame = true;
-			if (Constants.OFFLINE) {
+			if (Constants.OFFLINE)
+			{
 				controlled = new Mage();
 				controlled.setPos(new Vector2D(40, 40));
 				current[currentFloor].addPlayer(controlled);
@@ -206,12 +237,16 @@ public class ClientMain extends JFrame {
 			(new Thread() {
 				long lastUpdate;
 
-				public void run() {
+				public void run()
+				{
 					lastUpdate = System.currentTimeMillis();
-					while (inGame) {
+					while (inGame)
+					{
 						// Update player with client data
-						if (!Constants.OFFLINE) {
-							try {
+						if (!Constants.OFFLINE)
+						{
+							try
+							{
 								byteStream = new ByteArrayOutputStream();
 								os = new ObjectOutputStream(byteStream);
 								os.flush();
@@ -220,7 +255,8 @@ public class ClientMain extends JFrame {
 								byte[] object = byteStream.toByteArray();
 								byte[] message = new byte[object.length + 1];
 								message[0] = 3;
-								for (int i = 0; i < object.length; i++) {
+								for (int i = 0; i < object.length; i++)
+								{
 									message[i + 1] = object[i];
 								}
 								sock.send(
@@ -228,7 +264,8 @@ public class ClientMain extends JFrame {
 												message.length,
 												addr, Constants.SERVER_PORT));
 							}
-							catch (Exception e) {
+							catch (Exception e)
+							{
 								e.printStackTrace();
 							}
 						}
@@ -240,22 +277,26 @@ public class ClientMain extends JFrame {
 
 						int roomCheck = current[currentFloor]
 								.atDoor(controlled);
-						if (roomCheck == Constants.LEFT) {
+						if (roomCheck == Constants.LEFT)
+						{
 							current[currentFloor] = current[currentFloor]
 									.moveTo(current[currentFloor].getLeft(),
 											roomCheck);
 						}
-						else if (roomCheck == Constants.RIGHT) {
+						else if (roomCheck == Constants.RIGHT)
+						{
 							current[currentFloor] = current[currentFloor]
 									.moveTo(current[currentFloor].getRight(),
 											roomCheck);
 						}
-						else if (roomCheck == Constants.UP) {
+						else if (roomCheck == Constants.UP)
+						{
 							current[currentFloor] = current[currentFloor]
 									.moveTo(current[currentFloor].getUp(),
 											roomCheck);
 						}
-						else if (roomCheck == Constants.DOWN) {
+						else if (roomCheck == Constants.DOWN)
+						{
 							current[currentFloor] = current[currentFloor]
 									.moveTo(current[currentFloor].getDown(),
 											roomCheck);
@@ -264,10 +305,12 @@ public class ClientMain extends JFrame {
 						long time = System.currentTimeMillis();
 						long diff = time - lastUpdate;
 						lastUpdate = time;
-						try {
+						try
+						{
 							Thread.sleep(Math.max(0, 1000 / 60 - diff));
 						}
-						catch (Exception e) {
+						catch (Exception e)
+						{
 							e.printStackTrace();
 						}
 					}
@@ -275,8 +318,10 @@ public class ClientMain extends JFrame {
 			}).start();
 		}
 
-		private void parsePacket(DatagramPacket dp) throws Exception {
-			switch (dp.getData()[0]) {
+		private void parsePacket(DatagramPacket dp) throws Exception
+		{
+			switch (dp.getData()[0])
+			{
 			case 0: // connected
 				System.out.println("Connected to server");
 				break;
@@ -294,19 +339,22 @@ public class ClientMain extends JFrame {
 			case 3: // all player update
 				ois = makeObjectStream(dp.getData());
 				int numPlayers = ois.readInt();
-				for (int i = 0; i < numPlayers; i++) {
+				for (int i = 0; i < numPlayers; i++)
+				{
 					boolean exists = false;
 					Player p = (Player) ois.readObject();
 					for (int j = 0; j < current[currentFloor].getPlayers()
-							.size(); j++) {
-						if (current[currentFloor].getPlayers().get(j)
-								.getID() == p
-										.getID()) {
+							.size(); j++)
+					{
+						if (current[currentFloor].getPlayers().get(j).getID() == p
+								.getID())
+						{
 							current[currentFloor].getPlayers().set(j, p);
 							exists = true;
 						}
 					}
-					if (!exists) {
+					if (!exists)
+					{
 						current[currentFloor].getPlayers().add(p);
 					}
 				}
@@ -316,9 +364,11 @@ public class ClientMain extends JFrame {
 				ois = makeObjectStream(dp.getData());
 				controlled = (Player) ois.readObject();
 				for (int i = 0; i < current[currentFloor].getPlayers()
-						.size(); i++) {
+						.size(); i++)
+				{
 					if (current[currentFloor].getPlayers().get(i)
-							.getID() == controlled.getID()) {
+							.getID() == controlled.getID())
+					{
 						current[currentFloor].getPlayers().set(i, controlled);
 					}
 				}
@@ -328,14 +378,16 @@ public class ClientMain extends JFrame {
 		}
 
 		private ObjectInputStream makeObjectStream(byte[] bytes)
-				throws Exception {
+				throws Exception
+		{
 			ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
 			byteStream.read();
 			ObjectInputStream ois = new ObjectInputStream(byteStream);
 			return ois;
 		}
 
-		public void drawGame(Graphics g, Player p) {
+		public void drawGame(Graphics g, Player p)
+		{
 			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 					RenderingHints.VALUE_ANTIALIAS_ON);
 			Vector2D offset = Constants.MIDDLE.subtract(p.getPos());
@@ -350,7 +402,8 @@ public class ClientMain extends JFrame {
 			drawHUD(p, g);
 		}
 
-		public void drawHUD(Player p, Graphics g) {
+		public void drawHUD(Player p, Graphics g)
+		{
 			g.setColor(new Color(0, 0, 0, 127));
 			g.fillRect(0, getHeight() - 95, getWidth() - 450, 100);
 
@@ -374,8 +427,10 @@ public class ClientMain extends JFrame {
 			g.drawString((p.getLevel() + 1) + "", 37, getHeight() - 25);
 			g.drawRect(30, getHeight() - 40, 19, 19);
 
-			for (int i = 0; i < 3; i++) {
-				if (p.getAbilityActive(i + 1) > 0) {
+			for (int i = 0; i < 3; i++)
+			{
+				if (p.getAbilityActive(i + 1) > 0)
+				{
 					g.setColor(new Color(255, 255, 255, 127));
 					g.fillRect(340 + 80 * i, getHeight() - 85,
 							10 + SpriteSheet.HUD_IMAGES[p.getType()][i]
@@ -385,7 +440,8 @@ public class ClientMain extends JFrame {
 				}
 				g.drawImage(SpriteSheet.HUD_IMAGES[p.getType()][i],
 						345 + 80 * i, getHeight() - 80, null);
-				if (p.getCooldown(i + 1) > 0) {
+				if (p.getCooldown(i + 1) > 0)
+				{
 					g.setColor(new Color(255, 255, 255, 127));
 					g.fillArc(
 							345 + 80 * i,
@@ -396,15 +452,16 @@ public class ClientMain extends JFrame {
 									.getHeight(null),
 							90,
 							(int) (p.getCooldown(i + 1) * 1.0
-									/ Constants.AB_COOLDOWNS[p.getType()][i]
-									* 360));
+									/ Constants.AB_COOLDOWNS[p.getType()][i] * 360));
 				}
 			}
 		}
 
 		@Override
-		public void mousePressed(MouseEvent e) {
-			switch (e.getButton()) {
+		public void mousePressed(MouseEvent e)
+		{
+			switch (e.getButton())
+			{
 			case MouseEvent.BUTTON1:
 				cs.press(ControlState.KEY_ATTACK);
 				break;
@@ -415,8 +472,10 @@ public class ClientMain extends JFrame {
 		}
 
 		@Override
-		public void mouseReleased(MouseEvent e) {
-			switch (e.getButton()) {
+		public void mouseReleased(MouseEvent e)
+		{
+			switch (e.getButton())
+			{
 			case MouseEvent.BUTTON1:
 				cs.release(ControlState.KEY_ATTACK);
 				break;
@@ -427,18 +486,22 @@ public class ClientMain extends JFrame {
 		}
 
 		@Override
-		public void mouseMoved(MouseEvent e) {
+		public void mouseMoved(MouseEvent e)
+		{
 			cs.updateMouse(e.getPoint());
 		}
 
 		@Override
-		public void mouseDragged(MouseEvent e) {
+		public void mouseDragged(MouseEvent e)
+		{
 			cs.updateMouse(e.getPoint());
 		}
 
 		@Override
-		public void keyPressed(KeyEvent e) {
-			switch (e.getKeyCode()) {
+		public void keyPressed(KeyEvent e)
+		{
+			switch (e.getKeyCode())
+			{
 			case KeyEvent.VK_W:
 				cs.press(ControlState.KEY_UP);
 				break;
@@ -461,8 +524,10 @@ public class ClientMain extends JFrame {
 		}
 
 		@Override
-		public void keyReleased(KeyEvent e) {
-			switch (e.getKeyCode()) {
+		public void keyReleased(KeyEvent e)
+		{
+			switch (e.getKeyCode())
+			{
 			case KeyEvent.VK_W:
 				cs.release(ControlState.KEY_UP);
 				break;
@@ -485,19 +550,23 @@ public class ClientMain extends JFrame {
 		}
 
 		@Override
-		public void mouseClicked(MouseEvent e) {
+		public void mouseClicked(MouseEvent e)
+		{
 		}
 
 		@Override
-		public void mouseEntered(MouseEvent e) {
+		public void mouseEntered(MouseEvent e)
+		{
 		}
 
 		@Override
-		public void mouseExited(MouseEvent e) {
+		public void mouseExited(MouseEvent e)
+		{
 		}
 
 		@Override
-		public void keyTyped(KeyEvent e) {
+		public void keyTyped(KeyEvent e)
+		{
 		}
 	}
 }
